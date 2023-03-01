da05b9c18600bbc930eb7d16e8fa1ba6fe8fc4ab
Serz999 <serz_999@mail.ru>
2023-03-01 19:15:23 +0300

    Webdriver management and platform switching

--- src/catalogs_crawler/__main__.py
+++ src/catalogs_crawler/__main__.py
@@ -1,32 +1,50 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from sys import argv, platform
+import os
+import time
 import json
 from parser import get_catalog
 
 
 def main():
-    # install poetry for wsl
-    # need to wrap code in cli parser
-    # filename = input()
-    with open("../../targets.json") as f:
-        targets = json.load(f)
+    # TODO
+    #   how to invoke modul from ahy places and without interpretaitor name writing in console?
+    # TODO
+    #   install poetry for wsl
+    #   need to wrap code in cli parser
+    filename = argv[1]
+    with open(filename) as file:
+        targets = json.load(file)
 
-    # if linux os
-    # options = webdriver.ChromeOptions()
-    # driver_name = "../../chromedriver/chromedriver"
-    # if win os
-    options = webdriver.FirefoxOptions()
-    driver_name = "../../firefoxdriver/geckodriver.exe"
+    if platform == "linux" or platform == "linux2":
+        options = webdriver.ChromeOptions()
+        driver = ChromeDriverManager()
+        service = Service(driver.install())
+    elif platform == "win32":
+        options = webdriver.FirefoxOptions()
+        driver = GeckoDriverManager()
+        service = Service(driver.install())
     options.add_argument("--headless")
+
     browser = webdriver.Firefox(
-        executable_path=driver_name,
+        service=service,
         options=options
     )
+
     try:
         sites = targets["available"]
         for site in sites:
             print(f"-------------<{site['name']}>-------------\n")
             for link in site["links"]:
-                get_catalog(browser, site, link["src"], link["category"])
+                root_page = link["src"]
+                catalog_name = link["category"]
+                get_catalog(browser, site, root_page, catalog_name)
     except Exception as ex:
         print(ex)
     finally:
--- src/catalogs_crawler/parser.py
+++ src/catalogs_crawler/parser.py
@@ -1,20 +1,30 @@
 import time
+import os
+from typing import List, Dict
 from bs4 import BeautifulSoup
 
 
-def get_catalog(browser, website, page, output):
-    # Emulation of user action
-    # browser.get(page)
-    # cookies_1 = ...
-    # cookies_2 = ...
-    # cookies_3 = ...
-    # browser.add_cookie(cookies_1)
-    # browser.add_cookie(cookies_2)
-    # browser.add_cookie(cookies_3)
+def get_catalog(browser, site: Dict, initial_page, catalog_name) -> List:
+    products = list()
 
-    browser.get(page)
-    source_data = browser.page_source
+    # TODO
+    #   Emulation of user action
+    #   browser.get(page)
+    #   cookies_1 = ...
+    #   cookies_2 = ...
+    #   cookies_3 = ...
+    #   browser.add_cookie(cookies_1)
+    #   browser.add_cookie(cookies_2)
+    #   browser.add_cookie(cookies_3)
+    #
+    # TODO
+    #  Buy proxy???
 
-    soup = BeautifulSoup(source_data, 'html.parser')
+    browser.get(initial_page)
+    source = browser.page_source
 
-    pass
+    html = BeautifulSoup(source, 'html.parser')
+    # print(html.text)
+    return products
+
+
